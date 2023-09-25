6e2529f6573ded2423178b2cfe0a6ce679d6c5c9
Joey Palchak <joey.palchak@gmail.com>
2023-09-25 10:18:16 -0700

    create MadLibsVariable model

---
+++ MadLibs/Models/MadLibsVariable.cs
@@ -0,0 +1,16 @@
+
+namespace MadLibs.Models
+{
+  public class MadLibsVariable
+  {
+    // auto-implemented properties
+    public string Name { get; set; }
+    // Excuse
+    public string BodyPart {get; set; }
+    public string Fluid { get; set; }
+    public string Substance { get; set; }
+    // Hall Pass
+    public string Place { get; set; }
+    public string Noun { get; set; }
+  }
+}
